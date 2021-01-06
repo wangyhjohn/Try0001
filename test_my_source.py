52fcdd18d5541bfdecf1d4c02c91f0061b9338d3
wangyhjohn <66069983+wangyhjohn@users.noreply.github.com>
2021-01-06 20:55:26 +0100

    Update test_my_source.py

--- test_my_source.py
+++ test_my_source.py
@@ -1,1 +1,11 @@
-#test_my_source#
+import pytest
+from my_source import add_func
+
+
+def test_add_one():
+    assert add_func(2, 5) == 7
+
+
+@pytest.mark.xfail
+def test_add_fail():
+    assert add_func(5, 5) == 9
