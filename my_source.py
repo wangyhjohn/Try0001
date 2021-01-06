fca961602a0314a57d5e6bd4eaf1050756a4c09b
wangyhjohn <66069983+wangyhjohn@users.noreply.github.com>
2021-01-06 20:55:01 +0100

    Update my_source.py

--- my_source.py
+++ my_source.py
@@ -1,1 +1,12 @@
-#my_source#
+"""Contains code under test for solution_ci."""
+
+def add_func(a, var_two):
+    """
+    Add two variables.
+
+    Args
+        a: first variable
+        var_two: second variable
+
+    """
+    return a + var_two
